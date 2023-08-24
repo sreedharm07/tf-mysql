569cf9de8d2f313c7e1413fc4ca37336131ccd47
sreedhar <sreedhar70755@gmail.com>
2023-08-24 17:50:29 +0530

    tf

--- main.tf
+++ main.tf
@@ -37,8 +37,8 @@ resource "aws_rds_cluster" "main" {
   cluster_identifier               = "${local.name_pre}-cluster"
   db_subnet_group_name             = aws_db_subnet_group.main.name
   database_name                    = data.aws_ssm_parameter.dbname.value
-  master_username                  = data.aws_ssm_parameter.username.name
-  master_password                  = data.aws_ssm_parameter.password.name
+  master_username                  = data.aws_ssm_parameter.username.value
+  master_password                  = data.aws_ssm_parameter.password.value
   backup_retention_period          = var.backup_retention_period
   preferred_backup_window          = var.preferred_backup_window
   vpc_security_group_ids           = [aws_security_group.main.id]
